3f31985ca3305c4c11bf02ba7999292bedb01918
cheng.lingen <cheng.lingen@9c87f486-7f2d-4350-b948-b2028470fdc6>
2016-07-13 10:14:01 +0000

    7.4

--- o2o/trunk/prototype/html/家具商城/js/index.js
+++ o2o/trunk/prototype/html/家具商城/js/index.js
@@ -1,11 +1,22 @@
 ﻿$(function () {
+    head();
     //suspension();
     Timelimit();
     //PlacedTop(); //置顶
     prompt();
 
     Backswitch();
+
+    cityList();
 })
+function head() {
+    $(".Search-categories").mouseover(function () { $(".Search-categories>ul").show(); })
+    $(".Search-categories").mouseout(function () { $(".Search-categories>ul").hide(); })
+    $(".Search-categories ul li").click(function () {
+        var name = $(this).text();
+        $(".Search-categories>label").text(name); $(".Search-categories>ul").hide();
+    })
+}
 
 function suspension() {
     $(".nav-ul>ul>li:eq(0)").mouseover(function () { $(".nav-ul>ul>li>ul").css("display","block") })
@@ -27,39 +38,22 @@ function PlacedTop() {
         window.top.location = window.self.location;
     })
 }
-
 // 获取置顶对象
-
 var obj = document.getElementById('scroll');
-
 var scrollTop = null;
-
 // 置顶对象点击事件
-
 obj.onclick = function () {
-
     var timer = setInterval(function () {
-
         window.scrollBy(0, -100);
-
         if (scrollTop == 0)
-
             clearInterval(timer);
-
     }, 2);
-
 }
-
 // 窗口滚动检测
-
 window.onscroll = function () {
-
     scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-
     obj.parentNode.parentNode.style.display = (scrollTop >= 600) ? "block" : "none";
-
 }
-
 
 function prompt() {
     var top,text;
@@ -85,7 +79,31 @@ function Backswitch() {
     })
     
 }
-function set() {
-    
+
+
+
+function cityList() {
+    $(document).mouseover(function (e) {
+        var _con = $('.Switch-city,.hide_city_group');   // 设置目标区域
+        if (!_con.is(e.target) && _con.has(e.target).length === 0) { // Mark 1
+            $(".hide_city_group").hide();
+        } else {
+            $(".hide_city_group").show();
+        }
+    });
+
+    ///点击字母跳到
+    $("#JS_header_city_char a").bind("click", function () {
+        var anchorname = $(this).attr("anchorname");
+        var getchar = document.getElementById(anchorname);
+        if (getchar) {
+            getchar.scrollIntoView(false);
+        }
+    });
+
+    $(".hot_city a,.city_list .icity_names a").bind("click", function () {
+        var cityValue = $(this).text();
+        $(".city-name").text(cityValue);
+        $(".hide_city_group").hide();
+    })
 }
-setTimeout("")
